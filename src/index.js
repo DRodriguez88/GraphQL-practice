f48fe2fd2d10f9363067836064865d1e08c4c13f
Dj <rodriguezdanilo88@gmail.com>
2018-07-07 18:49:46 -0700

    Config for prisma and auth flow

--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const AuthPayload = require("./resolvers/AuthPayload");
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-
-let idCount = links.length;
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-  Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    }
-  }
+  Query,
+  Mutation,
+  AuthPayload
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint:
+        "https://us1.prisma.sh/danilo-rodriguez-78be06/graphQL-practice/dev",
+      debug: true
+    })
+  })
 });
 server.start(() => console.log(`Server is running on lo4000`));
